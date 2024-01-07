b571b194211143e2c1c84044113f8071b323b5c9
Luke LaFountaine <luke.lafountaine@gmail.com>
2024-01-07 13:42:39 -0500

    feat: append to bash history after each command

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -2,4 +2,11 @@ parse_git_branch() {
    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
+function prompt {
+   # Append to the bash history after each command rather than on exit
+   history -a
+}
+
+PROMPT_COMMAND='prompt'
+
 export PS1="\W \$(parse_git_branch) $ "
